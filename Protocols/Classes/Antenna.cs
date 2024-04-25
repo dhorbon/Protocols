4f2123e14b97a83d6cc66526b53fa5c71607c8dc
dhorbon <szymon080206@gmail.com>
2024-04-25 18:27:22 +0200

    Most of simulation logic

--- Protocols/Classes/Antenna.cs
+++ Protocols/Classes/Antenna.cs
@@ -7,22 +7,57 @@ namespace Protocols.Classes
         public float x;
         public float y;
 
-        public List<IProtocol?> protocols;
+        public string recieved = "";
 
-        
+        private Simulation simReference;
+
+        private int curProtocol;
+        public List<IProtocol> protocols;
 
         private List<Message> incomingMessages = new();
 
-        public Antenna(float x, float y, List<IProtocol?> protocols)
+        public Antenna(float x, float y, List<IProtocol> protocols, Simulation simulation, int curProtocol)
         {
             this.x = x;
             this.y = y;
             this.protocols = protocols;
+            simReference = simulation;
+            this.curProtocol = curProtocol;
         }
 
         public float DistanceTo(Antenna target)
         {
             return MathF.Sqrt(MathF.Pow(this.x + target.x, 2f) + MathF.Pow(this.y + target.y, 2f));
+        }
+
+        public void Tick()
+        {
+            int numOfSignals = 0;
+            int recievedSignal = 0;
+            for (int i = 0; i < incomingMessages.Count; i++)
+            {
+                if (incomingMessages[i].TryRecieve() != null)
+                {
+                    numOfSignals++;
+                    recievedSignal = i;
+                }
+            }
+
+            if (numOfSignals < 2 && numOfSignals != 0)
+            {
+                protocols[curProtocol].Recieve(incomingMessages[recievedSignal].TryRecieve());
+            }
+            else protocols[curProtocol].Tick();
+        }
+        
+        public void Recieve(Message m)
+        {
+            incomingMessages.Add(m);
+        }
+
+        public void Save(string content)
+        {
+            recieved += content;
         }
     }
 }
